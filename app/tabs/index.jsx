0bdffcb0cf7792e11715f9b567116452beace440
Mehtabhussain-5526 <mehtabhussain.atwork@gmail.com>
2025-10-15 20:14:01 +0500

    Basic code Base

---
+++ app/tabs/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import './../../global.css';
+const index = () => {
+  return (
+    <View >
+      <Text className="text-[30px] font-bold text-yellow-500">index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      require.resolve("expo-router/babel"),
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./",
+          },
+        },
+      ],
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-worklets/plugin",
+    ],
+  };
+};
